0eeb5c41361d6ac769bcf3d523b80c37ff648f2f
Andres Tamayo <avalenzuelatamayo@gmail.com>
2019-01-05 14:05:21 -0800

    Chapter 2 homework and scripts

---
+++ Chapter_2/ComputeDistanceGraphic.py
@@ -0,0 +1,22 @@
+import turtle
+
+x1,y1 = eval(input("Enter x1 and y1 for point 1: "))
+x2,y2 = eval(input("Enter x2 and y2 for point 2: "))
+
+distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+#Display the two points and the connecting line
+
+turtle.penup()
+turtle.goto(x1,y1)
+turtle.pendown()
+turtle.write("Point 1")
+turtle.goto(x2,y2)
+turtle.write("Point 2")
+
+#Move to the center point of the line
+turtle.penup()
+turtle.goto((x1 + x2) / 2,(y1 + y2) / 2)
+turtle.write(distance)
+
+turtle.done()
